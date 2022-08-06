2390ba7cf80e89d957f4098ec7efae204d8d94bb
Matthew Wong <66754344+MattWong-ca@users.noreply.github.com>
2022-08-05 22:48:51 -0400

    Update and rename FirstIncreasingThenDecreasingArrayMaxValueIndex.cs to FirstIncThenDecArrayMaxValueIndex.cs

--- FirstIncThenDecArrayMaxValueIndex.cs
+++ FirstIncThenDecArrayMaxValueIndex.cs
@@ -2,6 +2,11 @@
 // find the INDEX of the maximum value in the array. Similar to: 
 // https://www.geeksforgeeks.org/find-the-maximum-element-in-an-array-which-is-first-increasing-and-then-decreasing/
 
+// CLARIFYING QUESTIONS
+// - What if int array is empty?
+// - What if there are negative values?
+// - What if it's only a single value?
+// - How to turn from O(n) to O(log(n))?
 int MaxValueIndex( int[] intArray ) {
 
 	if ( intArray.Length == 0 ) { return -1; }

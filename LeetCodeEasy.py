adac01d3fa130772ab9fc68a0e61390336d177c6
Matt Wong | FOR HIRE <66754344+MattWong-ca@users.noreply.github.com>
2024-06-10 18:47:42 -0400

    Create LeetCodeEasy.py

---
+++ LeetCodeEasy.py
@@ -0,0 +1,7 @@
+# 1. Two Sum
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if target - nums[j] == nums[i] and i != j:
+                    return [i,j]

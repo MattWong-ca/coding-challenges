2aa78735f87a1efceb8bc3c473547946f318fb3e
Matt Wong | FOR HIRE <66754344+MattWong-ca@users.noreply.github.com>
2024-06-11 18:57:52 -0400

    Update LeetCodeEasy.py

--- LeetCodeEasy.py
+++ LeetCodeEasy.py
@@ -5,3 +5,17 @@ class Solution:
             for j in range(len(nums)):
                 if target - nums[j] == nums[i] and i != j:
                     return [i,j]
+
+# 121. Best Time to Buy and Sell Stock
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+        
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+        
+        return max_profit

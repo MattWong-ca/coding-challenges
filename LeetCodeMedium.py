44b565eb813a9d60f222fc0c2565551a8e669c70
Matt Wong | FOR HIRE <66754344+MattWong-ca@users.noreply.github.com>
2024-06-17 10:20:12 -0400

    Update LeetCodeMedium.py

--- LeetCodeMedium.py
+++ LeetCodeMedium.py
@@ -12,3 +12,26 @@ class Solution:
                 length = length + 1
         
         return length
+
+# 49. Group Anagrams
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Dictionary would be string --> string[]
+        #   "aet" --> ["tea", "eat", "ate"]
+        #   "ant" --> ["tan", "nat"]
+
+        my_dict = {}
+        # 1. Iterate through the array. For each string, sort it
+        for s in strs:
+            s_array = list(s)
+            s_array.sort()
+            x = "".join(s_array)
+            # If the sorted string isn't a key in the dictionary, add it and the current string
+            # Else the sorted string is already a key in the dictionary, so just append the current string to its value
+            # Ex: "eat" --> add "aet" value to dictionary, with "tea" as one of the values
+            if x not in my_dict:
+                my_dict[x] = [s]
+            else:
+                my_dict[x].append(s)
+        
+        return list(my_dict.values())

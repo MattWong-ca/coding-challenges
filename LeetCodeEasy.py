f8b4e26c741c75e716c567618055272a69a70590
Matt Wong <66754344+MattWong-ca@users.noreply.github.com>
2024-10-24 22:11:39 +0800

    Update LeetCodeEasy.py

--- LeetCodeEasy.py
+++ LeetCodeEasy.py
@@ -250,6 +250,23 @@ class Solution:
             n = sum
         return True
 
+# 205. Isomorphic Strings
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+
+        for char_s, char_t in zip(s,t):
+            if char_s in s_to_t and s_to_t[char_s] != char_t:
+                return False
+            if char_t in t_to_s and t_to_s[char_t] != char_s:
+                return False
+
+            s_to_t[char_s] = char_t
+            t_to_s[char_t] = char_s
+
+        return True
+
 # 206. Reverse Linked List 
 # Visualization: https://www.youtube.com/watch?v=TSDl7sRxWdU&ab_channel=QuinstonPimenta
 class Solution:

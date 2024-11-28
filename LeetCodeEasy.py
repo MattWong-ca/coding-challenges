10b430db324fb079ec933b74d7135bd5228d2bf3
Matt Wong <66754344+MattWong-ca@users.noreply.github.com>
2024-11-27 21:02:13 -0500

    Update LeetCodeEasy.py

--- LeetCodeEasy.py
+++ LeetCodeEasy.py
@@ -113,13 +113,20 @@ class Solution:
 
         return square_root
 
-# 70. Climbing Stairs (WORK IN PROGRESS, need to use DP)
+# 70. Climbing Stairs
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 1:
             return 1
     
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
 
 # 100. Same Tree
 class Solution:

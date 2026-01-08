8069d6f2ccd42ef9c8bfb363ea66b953b2052f37
Matt Wong <66754344+MattWong-ca@users.noreply.github.com>
2026-01-08 00:12:41 -0500

    Update LeetCodeEasy.py

--- LeetCodeEasy.py
+++ LeetCodeEasy.py
@@ -25,6 +25,29 @@ class Solution:
         reverse_x = str(x)[::-1]
         return x_str == reverse_x
 
+# 13. Roman to Integer
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        sum = 0
+
+        for i in range(len(s)):
+            if i+1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                sum -= roman[s[i]]
+            else:
+                sum += roman[s[i]]
+        
+        return sum
+
 # 14. Longest Common Prefix
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:

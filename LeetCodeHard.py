8e60ede5b544fa5f2bb6b59e0bb9407563f4ae26
Matt Wong <66754344+MattWong-ca@users.noreply.github.com>
2026-01-11 16:03:23 -0500

    Update LeetCodeHard.py

--- LeetCodeHard.py
+++ LeetCodeHard.py
@@ -20,3 +20,36 @@ class Solution:
         
         dfs(root)
         return self.max_sum
+
+# 329. Longest Increasing Path in a Matrix
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        mapping = {} # (r,c) --> #
+
+        def dfs(r, c, prevVal):
+            # 1. Check if in range or if larger than prevVal
+            if r < 0 or c < 0 or r == ROWS or c == COLS or matrix[r][c] <= prevVal:
+                return 0
+
+            # 2. Check if already in mapping
+            if (r,c) in mapping:
+                return mapping[(r,c)]
+
+            # 3. Check all four directions, add to mapping, then return result
+            res = -1
+            res = max(res, 1 + dfs(r+1, c, matrix[r][c]))
+            res = max(res, 1 + dfs(r, c+1, matrix[r][c]))
+            res = max(res, 1 + dfs(r-1, c, matrix[r][c]))
+            res = max(res, 1 + dfs(r, c-1, matrix[r][c]))
+
+            mapping[(r,c)] = res
+            return res
+
+        # For loop - call dfs at each position
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, -1)
+
+        # Return max value in mapping
+        return max(mapping.values())
